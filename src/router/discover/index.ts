7b99899b76bc11f0e32b3d7812a8a58903469509
corner <sptdfdzl@163.com>
2022-06-19 23:44:06 +0800

    chore: 拆分路由

---
+++ src/router/discover/index.ts
@@ -0,0 +1,25 @@
+
+import { RouteRecordRaw } from 'vue-router'
+import discoverMenus from '@/api/constants/discover'
+
+const Discover = () => import('@/views/discover/index.vue')
+
+// 根据菜单栏自动生成路由配置
+const discoverChildren = discoverMenus.reduce((pre, cur) => {
+  let child: RouteRecordRaw = {} as RouteRecordRaw
+  child.path = cur.link as string
+  child.component = () => import(`../../views/discover/pages/${cur.component}.vue`)
+  pre.push(child)
+  return pre
+}, [] as RouteRecordRaw[])
+
+export default [
+  {
+    path: '/discover', component: Discover, children: [
+      { path: '/discover' , redirect: '/discover/recommend' },
+      // { path: '/discover/recommend', component: Recommend },
+      // { path: '/discover/playlist', component: Playlist }
+      ...discoverChildren
+    ]
+  }
+]
